a8d0807aefd7f62cc37764a5593a71a9f79cba18
AlexMaitland <alex.k.maitland@gmail.com>
2023-04-26 08:52:01 +0000

    Add quiz questions to the js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let quizData = 
+    [ {
+        question: 'What is the largest animal in the world?',
+        answers: ['Blue Whale','African Elephant','Indian Elephant', 'Giant Sloth'],
+        correctAnswer: 'Blue Whale'
+    }, 
+    {
+        question: 'What is the Capital of the Isle of Man?',
+        answers: ['Castletown',  'Douglas',  'St Helier', 'Peel'],
+        correctAnswer: 'Douglas'
+    },
+    {
+        question: 'What is the tallest mountain in the world?',
+        answers: ['Mount Kilimanjaro', 'Mount Everest', 'Mount Fuji', 'Ben Nevis'],
+        correctAnswer: 'Mount Everest'
+    },
+    {
+        question: 'What is the largest planet in our solar system?',
+        answers: ['Mars', 'Venus', 'Saturn', 'Jupiter'],
+        correctAnswer: 'Jupiter'
+    },
+    {
+        question: 'What is the square root of 144?',
+        answers: ['14', '72', '12', '27'],
+        correctAnswer: '12'
+    }
+    ];
